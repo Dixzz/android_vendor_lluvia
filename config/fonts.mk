SuperiorOS/android_vendor_superior

vendor: fonts: Add some more fonts[1/3]
* Credit to Joseph

Change-Id: Ib4c0f3eb91b6118fa0921b8fcceca25ce0b81d8b
Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

config/fonts.mk
@@ -19,7 +19,7 @@ PRODUCT_COPY_FILES += \
     vendor/superior/prebuilt/fonts/gobold/GoboldBold-Italic.ttf:system/fonts/GoboldBold-Italic.ttf \
     vendor/superior/prebuilt/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
     vendor/superior/prebuilt/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf \
-	vendor/superior/prebuilt/fonts/neoneon/neoneon.ttf:system/fonts/Neoneon-Regular.ttf \
+    vendor/superior/prebuilt/fonts/neoneon/neoneon.ttf:system/fonts/Neoneon-Regular.ttf \
     vendor/superior/prebuilt/fonts/roadrage/road_rage.ttf:system/fonts/RoadRage-Regular.ttf \
     vendor/superior/prebuilt/fonts/snowstorm/snowstorm.ttf:system/fonts/Snowstorm-Regular.ttf \
     vendor/superior/prebuilt/fonts/vcrosd/vcr_osd_mono.ttf:system/fonts/ThemeableFont-Regular.ttf \
@@ -29,4 +29,9 @@ PRODUCT_COPY_FILES += \
     vendor/superior/prebuilt/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
     vendor/superior/prebuilt/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
     vendor/superior/prebuilt/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
-    vendor/superior/prebuilt/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+    vendor/superior/prebuilt/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf \
+    vendor/superior/prebuilt/fonts/ABCThru/ABCThru.ttf:system/fonts/ABCThru.ttf \
+    vendor/superior/prebuilt/fonts/anurati/anurati_regular.ttf:system/fonts/anurati_regular.ttf \
+    vendor/superior/prebuilt/fonts/Joost/JoostAMillionaireNF.ttf:system/fonts/JoostAMillionaireNF.ttf \
+    vendor/superior/prebuilt/fonts/Locust/Locust_Resistance.ttf:system/fonts/Locust_Resistance.ttf \
+    vendor/superior/prebuilt/fonts/Wallpoet/Wallpoet_regular.ttf:system/fonts/Wallpoet_regular.ttf

Dixzz/android_vendor_lluvia

config/fonts.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 The Superior OS Project
+# Copyright (C) 2018 The lluvia OS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,4 +18,9 @@ PRODUCT_COPY_FILES += \
     vendor/lluvia/prebuilt/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
     vendor/lluvia/prebuilt/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
     vendor/lluvia/prebuilt/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
-    vendor/lluvia/prebuilt/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+    vendor/lluvia/prebuilt/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf \
+    vendor/lluvia/prebuilt/fonts/ABCThru/ABCThru.ttf:system/fonts/ABCThru.ttf \
+    vendor/lluvia/prebuilt/fonts/anurati/anurati_regular.ttf:system/fonts/anurati_regular.ttf \
+    vendor/lluvia/prebuilt/fonts/Joost/JoostAMillionaireNF.ttf:system/fonts/JoostAMillionaireNF.ttf \
+    vendor/lluvia/prebuilt/fonts/Locust/Locust_Resistance.ttf:system/fonts/Locust_Resistance.ttf \
+    vendor/lluvia/prebuilt/fonts/Wallpoet/Wallpoet_regular.ttf:system/fonts/Wallpoet_regular.ttf
cc17406ea5208952fe8ffce1865a4c9d1c02a671
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-11-24 14:38:10 +0530

    vendor: fonts: Add some more fonts[1/3]
    * Credit to Joseph
    
    Change-Id: Ib4c0f3eb91b6118fa0921b8fcceca25ce0b81d8b
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 The Superior OS Project
+# Copyright (C) 2018 The lluvia OS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,4 +18,9 @@ PRODUCT_COPY_FILES += \
     vendor/lluvia/prebuilt/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
     vendor/lluvia/prebuilt/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
     vendor/lluvia/prebuilt/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
-    vendor/lluvia/prebuilt/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+    vendor/lluvia/prebuilt/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf \
+    vendor/lluvia/prebuilt/fonts/ABCThru/ABCThru.ttf:system/fonts/ABCThru.ttf \
+    vendor/lluvia/prebuilt/fonts/anurati/anurati_regular.ttf:system/fonts/anurati_regular.ttf \
+    vendor/lluvia/prebuilt/fonts/Joost/JoostAMillionaireNF.ttf:system/fonts/JoostAMillionaireNF.ttf \
+    vendor/lluvia/prebuilt/fonts/Locust/Locust_Resistance.ttf:system/fonts/Locust_Resistance.ttf \
+    vendor/lluvia/prebuilt/fonts/Wallpoet/Wallpoet_regular.ttf:system/fonts/Wallpoet_regular.ttf
